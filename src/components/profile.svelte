f3a5b94e33d75b57904410db4aafd5fce5c74303
vitalspace <myglobalsounds@gmail.com>
2024-09-11 16:23:17 -0600

    comments removed

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,10 +1,10 @@
 <div class="grid gap-y-4">
   <div class="flex justify-between space-x-4 place-items-center">
-    <h2 class="text-justify text-4xl">
+    <h2 class="text-justify text-2xl xl:text-4xl">
       I'm Vital. Fullstack Developer Mexico City Metropolitan Area.
     </h2>
     <img
-      class="w-40 h-40 bg-slate-200 border-2 border-purple-900 rounded-xl"
+      class="w-24 h-24 xl:w-40 xl:h-40 bg-slate-200 border-2 border-purple-900 rounded-xl"
       src="vital.png"
       alt=""
     />
@@ -63,87 +63,3 @@
     </p>
   </div>
 </div>
-
-<!-- <div class="text-justify flex flex-col gap-y-4 w-full">
-  <div class="flex space-x-4">
-    <h2 class="text-5xl">
-      I'm Vital. I live in CDMX, where I create things for the web.
-    </h2>
-    <img
-      class="w-48 bg-slate-200 border-2 border-purple-900 rounded-xl"
-      src="vital.png"
-      alt=""
-    />
-  </div>
-  <div>
-    <p class="">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam quibusdam
-      vitae ullam tempora porro fugiat cumque accusantium asperiores quae odit
-      consectetur expedita perferendis beatae, distinctio adipisci libero
-      quaerat. Eum vel nam neque magni eligendi deserunt tempore sit cupiditate
-      et ipsa, optio dolore velit vero soluta corporis, dolorum eius nemo
-      distinctio?
-    </p>
-  </div>
-
-  <div class="grid grid-cols-2 gap-4">
-    <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem numquam
-      excepturi, ullam, quaerat pariatur adipisci necessitatibus consectetur
-      explicabo reprehenderit perferendis sequi aperiam porro commodi doloribus
-      iure nobis cumque quos, sed temporibus fugiat ipsam consequatur.
-      Perferendis pariatur similique amet. Animi quae cum totam vero alias ab
-      optio doloremque quaerat tenetur ipsa.
-    </div>
-
-    <div class="flex flex-col gap-y-4 border-2 border-gray-400 rounded-md p-2">
-      <div class="flex place-items-center space-x-11">
-        <img class="w-8" src="briefcase.svg" alt="">
-        <h2 class="font-bold text-lg">Work & Education</h2>
-      </div>
-      
-      <li class="flex">
-        <div>
-          <img class="w-20 rounded-full" src="lar.jpg" alt="" />
-        </div>
-        <div class="flex justify-between place-items-center px-4 w-full">
-          <div>
-            <h2 class="font-bold">Lar</h2>
-            <p class="text-gray-400 text-sm">core-team</p>
-          </div>
-          <div>
-            <p class="text-gray-400 text-sm">2023 - Present</p>
-          </div>
-        </div>
-      </li>
-      <li class="flex">
-        <div>
-          <img class="w-20 rounded-full" src="bunland.png" alt="" />
-        </div>
-        <div class="flex justify-between place-items-center px-4 w-full">
-          <div>
-            <h2 class="font-bold">Bunland</h2>
-            <p class="text-gray-400 text-sm">core-team</p>
-          </div>
-          <div>
-            <p class="text-gray-400 text-sm">2022 - Present</p>
-          </div>
-        </div>
-      </li>
-      <li class="flex">
-        <div class="">
-          <img class="w-20 rounded-full" src="nation.svg" alt="" />
-        </div>
-        <div class="flex justify-between place-items-center px-4 w-full">
-          <div>
-            <h2 class="font-bold">C Nations</h2>
-            <p class="text-gray-400 text-sm">Game Developer</p>
-          </div>
-          <div>
-            <p class="text-gray-400 text-sm">2020 - Present</p>
-          </div>
-        </div>
-      </li>
-    </div>
-  </div>
-</div> -->
