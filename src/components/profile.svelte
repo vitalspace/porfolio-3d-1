d2a57a20d2cc24a127f59d8ec26e901fcf14daf8
vitalspace <myglobalsounds@gmail.com>
2024-09-11 16:16:39 -0600

    updated

--- src/components/profile.svelte
+++ src/components/profile.svelte
@@ -1,4 +1,70 @@
-<div class="text-justify flex flex-col gap-y-4">
+<div class="grid gap-y-4">
+  <div class="flex justify-between space-x-4 place-items-center">
+    <h2 class="text-justify text-4xl">
+      I'm Vital. Fullstack Developer Mexico City Metropolitan Area.
+    </h2>
+    <img
+      class="w-40 h-40 bg-slate-200 border-2 border-purple-900 rounded-xl"
+      src="vital.png"
+      alt=""
+    />
+  </div>
+
+  <div>
+    <p class="text-md">
+      I am a fullstack developer with over 5 years of experience, specializing
+      in creating interactive user interfaces and scalable backend solutions. My
+      work spans from developing modern web applications to building desktop
+      bots and hybrid mobile apps.
+    </p>
+  </div>
+
+  <div>
+    <h3 class="text-xl">Experience</h3>
+  </div>
+
+  <div class="space-y-2">
+    <span class="underline">
+      LarEngine | Core Team January 2024 - Present | Remote
+    </span>
+    <p class="text-sm">
+      At LarEngine, I contribute to the development of cutting-edge technologies
+      using C and Zig. My role involves leveraging Git and GitHub for efficient
+      version control. I am currently working on building a new JavaScript
+      runtime called Lar, utilizing JavaScriptCore libraries. Additionally, I am
+      designing and implementing a JavaScript interpreter from scratch in Zig,
+      focusing on creating the lexer, parser, and AST.
+    </p>
+  </div>
+
+  <div class="space-y-2">
+    <span class="underline">Fiverr | Freelance August 2019 - Present | Mexico City </span>
+    <p class="text-sm">
+      As a freelance developer, I have worked on a wide range of projects,
+      creating highly interactive user interfaces with frameworks like React,
+      Vue, and Svelte. On the backend, I have extensive experience with Node.js,
+      Express, MongoDB, Nest.js, and MySQL. I have also developed desktop
+      applications using Electron.js and hybrid mobile apps using React Native
+      Expo and Ionic Capacitor. My projects include e-commerce platforms,
+      inventory management systems, and real-time chat applications, providing
+      custom-tailored solutions to meet client needs.
+    </p>
+  </div>
+
+  <div class="space-y-2">
+    <span class="underline">Weku.io | Frontend Developer | March 2019 - December 2020 </span>
+    <p class="text-sm">
+      Remote At Weku.io, I was responsible for
+      maintaining and implementing new features on the main website, utilizing
+      technologies like Angular and Bootstrap to ensure a smooth user
+      experience. I also managed version control of the main codebase with Git
+      and GitHub, and worked with Steem.js to integrate blockchain technology
+      into the platform.
+    </p>
+  </div>
+</div>
+
+<!-- <div class="text-justify flex flex-col gap-y-4 w-full">
   <div class="flex space-x-4">
     <h2 class="text-5xl">
       I'm Vital. I live in CDMX, where I create things for the web.
@@ -80,4 +146,4 @@
       </li>
     </div>
   </div>
-</div>
+</div> -->
